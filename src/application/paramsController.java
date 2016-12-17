71aec953a9193f2eb9800e25fa9ca40cfa94c9a8
Mirish <mirishlaen@hotmail.com>
2016-12-17 14:54:11 +0200

    add choicebox

--- src/application/paramsController.java
+++ src/application/paramsController.java
@@ -8,22 +8,41 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 
 public class paramsController {
 
-    @FXML
-    private Button Start;
+	ObservableList<String> difficultyList = FXCollections
+			.observableArrayList("Easy", "Medium", "hard");
 
-    @FXML
-    void handleStart(ActionEvent event) throws IOException {
-    	Parent home_page_parent = FXMLLoader.load(getClass().getResource("ReversiScene.fxml"));
-    	Scene board = new Scene(home_page_parent);
-    	Stage board_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    	board_stage.setScene(board);
-    	board_stage.show();
-    	
-    }
+	@FXML
+	private Button Start;
+
+	@FXML
+	private ChoiceBox<String> Difficulty;
+	
+	@FXML 
+	private void initialize(){
+		Difficulty.setValue("Easy");
+		Difficulty.setItems(difficultyList);
+		
+		
+	}
+
+	@FXML
+	void handleStart(ActionEvent event) throws IOException {
+		Parent home_page_parent = FXMLLoader.load(getClass().getResource(
+				"ReversiScene.fxml"));
+		Scene board = new Scene(home_page_parent);
+		Stage board_stage = (Stage) ((Node) event.getSource()).getScene()
+				.getWindow();
+		board_stage.setScene(board);
+		board_stage.show();
+
+	}
 
 }
