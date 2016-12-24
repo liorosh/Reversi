b5c690a69b4657b5f312ea46420c78aca1848c78
liorosh <lior3103@gmail.com>
2016-12-25 00:00:46 +0200

    last changes
    moving more gui stuff to threads

--- src/application/paramsController.java
+++ src/application/paramsController.java
@@ -11,47 +11,115 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-
 public class paramsController {
 
 	ObservableList<String> difficultyList = FXCollections.observableArrayList(
-			"Easy", "Medium", "hard");
+			"2", "4", "6", "8");
 
-	@FXML
-	private Button Start;
+	ObservableList<String> AIfunctionList = FXCollections.observableArrayList(
+			"MiniMax", "Alpha-Beta");
 
-	@FXML
-	private ChoiceBox<String> Difficulty;
+	ObservableList<String> HeuristicList = FXCollections.observableArrayList(
+			"Maximum Disks","Corners", "Mobility");
 
-	@FXML
-	private RadioButton PlayWComputer;
+    @FXML
+    private ChoiceBox<String> AIfunctionP2;
 
-	@FXML
-	private RadioButton PlayWPlayer;
+    @FXML
+    private ChoiceBox<String> AIfunctionP1;
 
-	@FXML
-	private RadioButton computerWcomputer;
+    @FXML
+    private ToggleGroup SelectPlayer;
 
-	@FXML
-	private ToggleGroup SelectPlayer;
+    @FXML
+    private Pane player1;
+
+    @FXML
+    private Button Start;
+
+    @FXML
+    private Pane player2;
+    @FXML
+    private RadioButton PlayWPlayer;
+
+    @FXML
+    private ChoiceBox<String> Difficulty;
+
+    @FXML
+    private ChoiceBox<String> Heuristic2;
+
+    @FXML
+    private RadioButton computerWcomputer;
+
+    @FXML
+    private ChoiceBox<String> Heuristic1;
+
+    @FXML
+    private RadioButton PlayWComputer;
 
 	@FXML
 	private void initialize() {
-		Difficulty.setValue("Easy");
-		Difficulty.setItems(difficultyList);
-		PlayWComputer.setSelected(true);
 
+		Difficulty.setItems(difficultyList);
+		Difficulty.setValue("2");
+		AIfunctionP1.setItems(AIfunctionList);
+		AIfunctionP1.setValue("MiniMax");
+		AIfunctionP2.setItems(AIfunctionList);
+		AIfunctionP2.setValue("MiniMax");
+		Heuristic1.setItems(HeuristicList);
+		Heuristic2.setItems(HeuristicList);
+		Heuristic1.setValue("Maximum Disks");
+		Heuristic2.setValue("Maximum Disks");
 	}
+	 @FXML
+	    void onclick(ActionEvent event) {
+		 String playerOrComputer=SelectPlayer.getSelectedToggle()
+					.toString()
+					.substring(
+							SelectPlayer.getSelectedToggle().toString()
+									.lastIndexOf("id=") + 3).split(",")[0];
+			switch(playerOrComputer){
+			case "PlayWComputer":
+			 /*Heuristic1.setDisable(true);
+			 Heuristic2.setDisable(false);
+			 AIfunctionP1.setDisable(true);*/
+				this.player2.setDisable(false);
+				this.player1.setDisable(true);
+				this.Start.setDisable(false);
+				this.Difficulty.setDisable(false);
+			 break;
+			case "PlayWPlayer":
+				/*Heuristic1.setDisable(true);
+				Heuristic2.setDisable(true);
+				AIfunctionP1.setDisable(true);*/
+				this.player1.setDisable(true);
+				this.player2.setDisable(true);
+				this.Start.setDisable(false);
+				this.Difficulty.setDisable(true);
+				break;
+			case "computerWcomputer":
+				/*Heuristic1.setDisable(false);
+				Heuristic2.setDisable(false);
+				AIfunction.setDisable(false);*/
+				this.player1.setDisable(false);
+				this.player2.setDisable(false);
+				this.Start.setDisable(false);
+				this.Difficulty.setDisable(false);
+				break;
+
+			}
+	    }
 
 	@FXML
 	void handleStart(ActionEvent event) throws IOException {
 		String playerOrComputer = null;
 		int nplayerOrComputer = 0;
-		String DifficultyChoice = Difficulty.getValue();
+		String DifficultyChoice = Difficulty.getValue().toString();
 		if (SelectPlayer.getSelectedToggle() != null) {
 			playerOrComputer = SelectPlayer
 					.getSelectedToggle()
@@ -66,6 +134,7 @@ public class paramsController {
 			if (playerOrComputer.equals("computerWcomputer"))
 				nplayerOrComputer = 3;
 		}
+
 		FXMLLoader loader = new FXMLLoader();
 		Parent home_page_parent = loader.load(getClass().getResource(
 				"ReversiScene.fxml").openStream());
@@ -73,6 +142,11 @@ public class paramsController {
 				.getController();
 		reversicontroller.getDifficulty(DifficultyChoice);
 		reversicontroller.getPlayerOrComputer(nplayerOrComputer);
+		reversicontroller.getAiFunction(AIfunctionP1.getValue());
+		reversicontroller.getAiFunction2(AIfunctionP2.getValue());
+		reversicontroller.GetHeuristic1(Heuristic1.getValue());
+		reversicontroller.GetHeuristic2(Heuristic2.getValue());
+		reversicontroller.initializeBoard();
 		Scene board = new Scene(home_page_parent);
 		Stage board_stage = (Stage) ((Node) event.getSource()).getScene()
 				.getWindow();
